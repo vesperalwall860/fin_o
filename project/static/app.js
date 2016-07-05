170d34d7968e197f3569ecdc8f8b52153410dd83
vesperalwall860 <vesperalwall860@gmail.com>
2016-07-05 10:01:21 +0300

    Change the delimiter notation for Angular and remove the  using

--- project/static/app.js
+++ project/static/app.js
@@ -2,13 +2,18 @@
     'use strict';
     var app = angular.module('FinApp', ['ngRoute']);
 
-    app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: '../static/fin_o.html',
-                controller: 'GreetingController'
-            })
-    }])
+    app.config(['$interpolateProvider', function($interpolateProvider) {
+      $interpolateProvider.startSymbol('{a');
+      $interpolateProvider.endSymbol('a}');
+    }]);
+
+    // app.config(['$routeProvider', function($routeProvider) {
+    //     $routeProvider
+    //         .when('/', {
+    //             templateUrl: '../static/fin_o.html',
+    //             controller: 'GreetingController'
+    //         })
+    // }])
     
     app.controller('GreetingController', ['$scope', function($scope) {
         $scope.greeting = 'Hola!';
